b6b31000742c6e5d11c4e0816ca8cae04c0cbdb8
Duc Anh <anhduong764027@gmail.com>
2023-10-10 18:30:11 +0700

    Worker

---
+++ src/main/java/test/Worker.java
@@ -0,0 +1,12 @@
+
+
+package test;
+
+import controller.WorkerController;
+public class Worker {
+
+    public static void main(String[] args) {
+        WorkerController wk = new WorkerController();
+        wk.run();
+    }
+}
